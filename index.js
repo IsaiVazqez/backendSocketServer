711148083aea1c881432c78164eb21cbaa151cbe
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-05-19 19:15:22 -0500

    Backend server versión 0.5

--- index.js
+++ index.js
@@ -2,38 +2,29 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-//App de Express
+// App de Express
 const app = express();
 
-//node server
-
-const server = require('http'). createServer(app);
-const io = require('socket.io')(server);
-
-//Mensajes de sockets
-io.on('connection', client => {
-    console.log('Cliente conectado');
-
-    client.on('disconnect', () => { 
-        console.log('Cliente desconectado');
-  });
-
-  client.on('mensaje', (payload) => { 
-      console.log('Mensaje!!', payload);
-        io.emit('mensaje', {admin: 'Nuevo mensaje'});
-  })
-});
-
-//Path Público
-
-const publicPath = path.resolve(__dirname, 'public' );
-
-app.use( express.static(publicPath));
+// Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
 
 
-server.listen(process.env.PORT, (err) => {
 
-    if( err ) throw new Error(err);
-    
-    console.log('Servidor corriendo en puerto!!!', process.env.PORT);
+
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+
+
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
 });
---
+++ sockets/socket.js
@@ -0,0 +1,57 @@
+const { io }= require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+
+const bands = new Bands();
+//console.log('init server')
+
+bands.addBand(new Band('Arctic'));
+bands.addBand(new Band('Waka'));
+bands.addBand(new Band('Interpol'));
+bands.addBand(new Band('Mang000'));
+
+console.log(bands);
+
+//Mensajes de sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado');
+  });
+
+  client.on('mensaje', (payload) => { 
+      console.log('Mensaje!!', payload);
+        io.emit('mensaje', {admin: 'Nuevo mensaje'});
+  });
+
+
+  client.on('emitir-mensaje', (payload) => {
+    /* console.log(payload) */
+    /* io.emit('nuevo-mensaje', payload); */ //Emite a todos
+    client.broadcast.emit('nuevo-mensaje', payload);  //Emite a todos menos quién lo emitó
+  })
+
+
+  client.on('vote-band', (payload) => {
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('add-band', (payload) => {
+    console.log(payload);
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('delete-band', (payload) => {
+
+    bands.deleteBand( payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+
+});
